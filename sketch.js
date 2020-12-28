b6b8303a19d3c70103ab7924dfee22bf2caeff6c
shravyabilla <shravya.billa12@gmail.com>
2020-12-27 19:47:13 -0600

    added constraintLog

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -34,7 +35,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    constraintLog = new Log(230,180,80,PI/2);
+
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body, constraintLog.body);
 
 }
 
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constraintLog.display();
+
+    chain.display();
 }
